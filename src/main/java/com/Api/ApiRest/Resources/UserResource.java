aa16df9743bc84bca0baa44eef45b8fd4dcf1192
Marcos Henrique Anschau <marcosanschau@gmail.com>
2022-07-02 13:36:32 -0300

    User entity e Resource

---
+++ src/main/java/com/Api/ApiRest/Resources/UserResource.java
@@ -0,0 +1,21 @@
+package com.Api.ApiRest.Resources;
+
+import java.time.LocalDate;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.Api.ApiRest.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll() {
+		User u = new User(1L, "Maria", "03680853092", LocalDate.now(), "992984130");
+		return ResponseEntity.ok().body(u);
+	}
+}
