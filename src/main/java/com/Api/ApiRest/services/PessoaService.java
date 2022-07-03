db52be7feb9c994322c9cf6546468497895c9fa9
Marcos Henrique Anschau <marcosanschau@gmail.com>
2022-07-03 13:03:10 -0300

    Tratamento de excecao delete

--- src/main/java/com/Api/ApiRest/services/PessoaService.java
+++ src/main/java/com/Api/ApiRest/services/PessoaService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.Api.ApiRest.Repositories.PessoaRepository;
 import com.Api.ApiRest.entities.Pessoa;
+import com.Api.ApiRest.services.exceptions.DatabaseException;
 import com.Api.ApiRest.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -30,7 +33,14 @@ public class PessoaService {
 	}
 	
 	public void delete(Long id) {
+		try {
+			
 		repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 	
 	
